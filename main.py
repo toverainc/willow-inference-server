b3bfe87fd4a47aad944d3a528151425bf682d6df
Kristian Kielhofner <kris@tovera.com>
2023-02-02 05:46:55 +0000

    We actually want to see errors

--- main.py
+++ main.py
@@ -64,19 +64,17 @@ async def root():
 
 @app.post("/api/infer")
 async def infer(request: Request, file: UploadFile, response: Response, model: Optional[str] = triton_model):
-    try:
-        # Setup access to file
-        img = io.BytesIO(await file.read())
-        triton_model = model
-        response, infer_time = do_infer(img, model)
-        print(response)
-        # Build JSON - NASTY
-        response_split = response.split(':', -1)
-        prob = response_split[0]
-        index = response_split[1]
-        label = response_split[2]
-        finalResponse = {'model': model, 'prob': prob, 'index': index, 'label': label, 'infer_time': infer_time}
-        json_compatible_item_data = jsonable_encoder(finalResponse)
-        return JSONResponse(content=json_compatible_item_data)
-    except:
-        return {'status': 'inference failed'}
+
+    # Setup access to file
+    img = io.BytesIO(await file.read())
+    triton_model = model
+    response, infer_time = do_infer(img, model)
+    print(response)
+    # Build JSON - NASTY
+    response_split = response.split(':', -1)
+    prob = response_split[0]
+    index = response_split[1]
+    label = response_split[2]
+    finalResponse = {'model': model, 'prob': prob, 'index': index, 'label': label, 'infer_time': infer_time}
+    json_compatible_item_data = jsonable_encoder(finalResponse)
+    return JSONResponse(content=json_compatible_item_data)
