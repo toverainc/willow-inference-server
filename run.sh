1a606887983b855cb1b485c20e88f64f1425a6bf
Kristian Kielhofner <kris@tovera.com>
2023-02-09 16:19:06 +0000

    Support GPU in run and listen on wildcard

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# We need to pass docker socket so it can launch containers for model-navigator
-docker run --rm -it -p 127.0.0.5:8000:8000 air-infer-api:latest
+docker run --rm -it --gpus all --shm-size=1g --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+    -v $PWD:/app -v $PWD/cache:/root/.cache \
+    -p 19000:8000 air-infer-api:latest \
+    uvicorn main:app --host 0.0.0.0 --port 8000 --reload
