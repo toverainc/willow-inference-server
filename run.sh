07fbe426d7580d8b18eec40e52d7083b5654e96b
Kristian Kielhofner <kris@tovera.com>
2023-02-09 21:03:46 +0000

    Allow easy control of CUDA devices in run

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+export CUDA_VISIBLE_DEVICES=0,1
+
 docker run --rm -it --gpus all --shm-size=1g --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-    -v $PWD:/app -v $PWD/cache:/root/.cache \
+    -v $PWD:/app -v $PWD/cache:/root/.cache -e CUDA_VISIBLE_DEVICES \
     -p 19000:8000 air-infer-api:latest \
     uvicorn main:app --host 0.0.0.0 --port 8000 --reload
