7d713288daa65eea309e1950e325829096ae6a0e
Kristian Kielhofner <kris@tovera.com>
2023-02-14 22:57:00 +0000

    First stab at docker-compose + traefik #4

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PORT="19000"
 
 if [ "$1" ]; then
     export CUDA_VISIBLE_DEVICES="$1"
@@ -10,4 +11,4 @@ fi
 docker run --rm -it --gpus all --shm-size=1g --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
     -v $PWD:/app -v $PWD/cache:/root/.cache -e CUDA_VISIBLE_DEVICES --name air-infer-api \
     --net host air-infer-api:latest \
-    uvicorn main:app --host 0.0.0.0 --port 19000 --reload --ssl-keyfile="/app/key.pem" --ssl-certfile="/app/cert.pem"
+    uvicorn main:app --host 0.0.0.0 --port "$PORT" --reload --ssl-keyfile="/app/key.pem" --ssl-certfile="/app/cert.pem"
