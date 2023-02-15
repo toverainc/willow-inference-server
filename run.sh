8be3378ae98cc4337258a2abefbd9c21c89fe5e2
Kristian Kielhofner <kris@tovera.com>
2023-02-15 18:44:52 +0000

    Initial testing with workers - currently ignored because we pass --reload

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 PORT="19000"
 
+# Currently loads four copies of all models... Hopefully there's a better way.
+export WEB_CONCURRENCY="4"
+
 if [ "$1" ]; then
     export CUDA_VISIBLE_DEVICES="$1"
 else
@@ -9,6 +12,7 @@ fi
 
 #     -p 19000:8000 -p 8081:8080 
 docker run --rm -it --gpus all --shm-size=1g --ipc=host \
-    -v $PWD:/app -v $PWD/cache:/root/.cache -e CUDA_VISIBLE_DEVICES --name air-infer-api \
+    -v $PWD:/app -v $PWD/cache:/root/.cache -e CUDA_VISIBLE_DEVICES -e WEB_CONCURRENCY \
+    --name air-infer-api \
     --net host air-infer-api:latest \
     uvicorn main:app --host 0.0.0.0 --port "$PORT" --reload --ssl-keyfile="/app/key.pem" --ssl-certfile="/app/cert.pem"
