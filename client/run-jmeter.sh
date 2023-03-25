ca090484293290b65c20cd548473fb14a80b0e0d
Kristian Kielhofner <kris@tovera.com>
2023-03-25 13:46:33 -0500

    Much better jmeter based on curl import

--- client/run-jmeter.sh
+++ client/run-jmeter.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ "$1" ]; then
+  THREADS="$1"
+else
+  THREADS="10"
+fi
+
 rm -rf jmeter-results.txt jmeter-report
 
-jmeter -n -t jmeter-asr-3sec.jmx -l jmeter-results.txt -e -o jmeter-report
+echo "Running jmeter with $THREADS threads"
+
+jmeter -n -t jmeter-asr.jmx -Jthreads="$THREADS" -Jrampup=10 -Jiterations=10 -Jfile=10sec.flac -l jmeter-results.txt -e -o jmeter-report
