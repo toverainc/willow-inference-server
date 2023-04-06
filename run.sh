bb853592dc82228f48f5caa7966341d1379bb877
Kristian Kielhofner <kris@tovera.com>
2023-04-06 11:49:36 -0700

    Move more gunicorn cmdline opts to vars

--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ IMAGE=${IMAGE:-air-infer-api}
 # Listen port
 LISTEN_PORT="19000"
 
+# Log level
+LOG_LEVEL="info"
+
 # Media port range
 # WebRTC dynamically negotiates UDP ports to for each session
 # You should keep this as small as possible for WebRTC connections
@@ -16,6 +19,9 @@ LISTEN_IP="0.0.0.0"
 
 # GPUS
 GPUS="all"
+
+# Allow forwarded IPs. This is a list of hosts to allow parsing of X-Forwarded headers from
+FORWARDED_ALLOW_IPS="127.0.0.1"
 
 # API Key - if defined all requests will require the X-Api-Key header with the configured value
 if [ -r .api_key ]; then
@@ -41,4 +47,4 @@ docker run --rm -it --gpus "$GPUS" --shm-size=64g --ipc=host \
     -v $PWD:/app -v $PWD/cache:/root/.cache  --env-file .env \
     --name air-infer-api \
     -p "$LISTEN_IP":"$LISTEN_PORT":"$LISTEN_PORT" -p "$MEDIA_PORT_RANGE":"$MEDIA_PORT_RANGE"/udp "$IMAGE":"$TAG" \
-    gunicorn main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:"$LISTEN_PORT" --graceful-timeout 10 --forwarded-allow-ips '*' --log-level debug -t 0
+    gunicorn main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:"$LISTEN_PORT" --graceful-timeout 10 --forwarded-allow-ips "$FORWARDED_ALLOW_IPS" --log-level "$LOG_LEVEL" -t 0
