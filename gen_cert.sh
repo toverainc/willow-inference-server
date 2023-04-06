7e522da478336e086c4d865bca84c466afaafc0f
Kristian Kielhofner <kris@tovera.com>
2023-04-06 12:13:16 -0700

    Cleanup old vision stuff and self-signed certs

--- gen_cert.sh
+++ gen_cert.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj '/CN=asr.air.tovera.io'
+if [ -z "$1" ]; then
+    echo "You need to provide your domain/common name"
+    exit 1
+fi
+
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/CN=$1"
