298094519314f46ba14b51fdc3e00cf199ac0e88
Kristian Kielhofner <kris@tovera.com>
2023-04-20 16:27:10 -0700

    Revert "Merge branch 'chatbot' into main"
    This reverts commit 39d581bee8da4e1799c16729a69cb33defa1df4c, reversing
    changes made to bc92506628bf74e9fb69a62e4ff4a2db4f4204cf.

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run --rm -it --gpus all --shm-size=1g --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-    -v $PWD:/app -v $PWD/cache:/root/.cache -v "/data":"/data" air-infer-api:latest \
+    -v $PWD:/app -v $PWD/cache:/root/.cache air-infer-api:latest \
     /bin/bash
