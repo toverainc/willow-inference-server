a6f3d9424c1581176473cdf993f683514f2fa236
freethenation <rpklafter@yahoo.com>
2023-04-19 21:37:45 -0400

    run.sh now uses env file

--- run.sh
+++ run.sh
@@ -3,37 +3,6 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
-# Which docker image to run
-IMAGE=${IMAGE:-air-infer-api}
-
-# Listen port
-LISTEN_PORT="19000"
-
-# Log level - acceptable values are debug, info, warning, error, critical. Suggest info or debug.
-LOG_LEVEL="info"
-
-# Media port range
-# WebRTC dynamically negotiates UDP ports for each session
-# You should keep this as small as possible for expected WebRTC connections
-MEDIA_PORT_RANGE="10000-10300"
-
-# Listen IP
-LISTEN_IP="0.0.0.0"
-
-# GPUS
-GPUS="all"
-
-# Allow forwarded IPs. This is a list of hosts to allow parsing of X-Forwarded headers from
-FORWARDED_ALLOW_IPS="127.0.0.1"
-
-# Shared memory size for docker
-SHM_SIZE="1gb"
-
-# API Key - if defined all requests will require the X-Api-Key header with the configured value
-if [ -r .api_key ]; then
-    API_KEY=$(cat .api_key)
-fi
-
 # Test for local environment file and use any overrides
 if [ -r .env ]; then
     echo "Startup - using configuration overrides from .env file"
@@ -43,12 +12,38 @@ else
     touch .env
 fi
 
-# TODO: Improve cmdline args
-if [ "$1" ]; then
-    TAG="$1"
-else
-    TAG="latest"
-fi
+#Import source the .env file
+set -a
+source .env
+set +a
+
+# Which docker image to run
+IMAGE=${IMAGE:-air-infer-api}
+
+# Listen port
+LISTEN_PORT=${LISTEN_PORT:-19000}
+
+# Log level - acceptable values are debug, info, warning, error, critical. Suggest info or debug.
+LOG_LEVEL=${LOG_LEVEL:-info}
+
+# Media port range
+# WebRTC dynamically negotiates UDP ports for each session
+# You should keep this as small as possible for expected WebRTC connections
+MEDIA_PORT_RANGE=${MEDIA_PORT_RANGE:-10000-10300}
+
+# Listen IP
+LISTEN_IP=${LISTEN_IP:-0.0.0.0}
+
+# GPUS
+GPUS=${GPUS:-"all"}
+
+# Allow forwarded IPs. This is a list of hosts to allow parsing of X-Forwarded headers from
+FORWARDED_ALLOW_IPS=${FORWARDED_ALLOW_IP:-127.0.0.1}
+
+# Shared memory size for docker
+SHM_SIZE=${SHM_SIZE:-1gb}
+
+TAG=${TAG:-latest}
 
 # Temp for hacky sallow config
 mkdir -p audio
