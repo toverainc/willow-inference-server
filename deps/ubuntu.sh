a2b0f0d32ee5ffb8285069cb0062ed1bde217d75
Kristian Kielhofner <kris@tovera.com>
2023-05-23 13:59:29 +0000

    Add Ubuntu install script

---
+++ deps/ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+ # Install Docker
+curl https://get.docker.com | sh && sudo systemctl --now enable docker
+
+ # Install NVIDIA Container Toolkit 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+
+sudo apt-get install -y nvidia-container-toolkit
+
+sudo nvidia-ctk runtime configure --runtime=docker
+
+# Restart Docker
+sudo systemctl restart docker
