8915af086e17b8658ed7d4fc10536e4766b8c9a2
Kristian Kielhofner <kris@tovera.com>
2023-05-27 13:32:48 -0500

    Standardize arguments and don't autorun missing commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,16 @@ set -e
 # Log level - acceptable values are debug, info, warning, error, critical. Suggest info or debug.
 LOG_LEVEL=${LOG_LEVEL:-info}
 
+FORWARDED_ALLOW_IPS=${FORWARDED_ALLOW_IPS:-127.0.0.1}
+
 set +a
 
 # Temp for hacky willow config
 mkdir -p nginx/static/audio
 
-if [ ! -d models ]; then
-    echo "Models not found. Downloading, please wait..."
-    ./utils.sh download_models
-fi
-
 # Make sure we have it just in case
 mkdir -p custom_speakers
 
 gunicorn main:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:19000 \
-    --graceful-timeout 10 --forwarded-allow-ips "*" --log-level "$LOG_LEVEL" -t 0 --keep-alive 3600
+    --graceful-timeout 10 --forwarded-allow-ips "$FORWARDED_ALLOW_IPS" --log-level "$LOG_LEVEL" \
+    -t 0 --keep-alive 3600
