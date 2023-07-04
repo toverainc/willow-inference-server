9c183a8c82345f0225cc693ea53ed2396323366f
Kristian Kielhofner <kris@tovera.com>
2023-07-04 07:50:20 -0500

    Set default loglevel to debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Log level - acceptable values are debug, info, warning, error, critical. Suggest info or debug.
-LOG_LEVEL=${LOG_LEVEL:-info}
+LOG_LEVEL=${LOG_LEVEL:-debug}
 
 FORWARDED_ALLOW_IPS=${FORWARDED_ALLOW_IPS:-127.0.0.1}
 
