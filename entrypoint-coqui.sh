5e772ce685388503d691a39b824f2e6fcf5f35cb
Kristian Kielhofner <kris@tovera.com>
2023-10-30 16:21:31 -0500

    Supress warning from coqui on generation

--- entrypoint-coqui.sh
+++ entrypoint-coqui.sh
@@ -7,6 +7,10 @@ else
     COQUI_CUDA="true"
 fi
 
+# Fix/suppress cudnn warning to not confuse people
+ln -sf /usr/local/lib/python3.10/dist-packages/torch/lib/libnvrtc-*.so.11.2 \
+    /usr/local/lib/python3.10/dist-packages/torch/lib/libnvrtc.so
+
 if [ "$TTS_MODEL_NAME" ]; then
     echo "Using coqui model $TTS_MODEL_NAME"
     python3 TTS/server/server.py --model_name "$TTS_MODEL_NAME" --use_cuda "$COQUI_CUDA"
