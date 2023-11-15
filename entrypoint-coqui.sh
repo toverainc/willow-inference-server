041715af2c0cfdb7b0a077d6fe58cb3702b05896
Kristian Kielhofner <kris@tovera.com>
2023-11-15 16:40:53 -0600

    Add support for Coqui XTTS

--- entrypoint-coqui.sh
+++ entrypoint-coqui.sh
@@ -7,14 +7,22 @@ else
     COQUI_CUDA="true"
 fi
 
-# Fix/suppress cudnn warning to not confuse people
-ln -sf /usr/local/lib/python3.10/dist-packages/torch/lib/libnvrtc-*.so.11.2 \
-    /usr/local/lib/python3.10/dist-packages/torch/lib/libnvrtc.so
+export COQUI_TOS_AGREED=1
 
-if [ "$TTS_MODEL_NAME" ]; then
-    echo "Using coqui model $TTS_MODEL_NAME"
-    python3 TTS/server/server.py --model_name "$TTS_MODEL_NAME" --use_cuda "$COQUI_CUDA"
+if [ -r "/xtts/main.py" ]; then
+    echo "Starting coqui xtts"
+    cd /xtts
+    uvicorn main:app --host 0.0.0.0 --port 5002
 else
-    echo "Using default coqui model"
-    python3 TTS/server/server.py --use_cuda "$COQUI_CUDA"
+    # Fix/suppress cudnn warning to not confuse people
+    ln -sf /usr/local/lib/python3.10/dist-packages/torch/lib/libnvrtc-*.so.11.2 \
+        /usr/local/lib/python3.10/dist-packages/torch/lib/libnvrtc.so
+
+    if [ "$TTS_MODEL_NAME" ]; then
+        echo "Using coqui model $TTS_MODEL_NAME"
+        python3 TTS/server/server.py --model_name "$TTS_MODEL_NAME" --use_cuda "$COQUI_CUDA"
+    else
+        echo "Using default coqui model"
+        python3 TTS/server/server.py --use_cuda "$COQUI_CUDA"
+    fi
 fi
